a4bb7505cc315cba4b02ad21c5e3bf4dd11d9049
Viktor <viktorhrimli101@gmail.com>
2024-07-29 14:03:57 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => {
     connection: {
       client: env("DATABASE_CLIENT"),
       connection: {
-        // host: process.env.DATABASE_HOST,
-        // port: process.env.DATABASE_PORT,
-        socketPath: process.env.INSTANCE_UNIX_SOCKET,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        // socketPath: process.env.INSTANCE_UNIX_SOCKET,
         database: process.env.DATABASE_NAME,
         user: process.env.DATABASE_USERNAME,
         password: process.env.DATABASE_PASSWORD,
